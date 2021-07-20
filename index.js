4a4ea48e485efc378d0768675ac3ee009159e2d3
xuannga <nguyenx206@gmail.com>
2021-07-19 19:16:40 -0700

    added prompts for manager and teams

---
+++ index.js
@@ -0,0 +1,65 @@
+//dependencies
+const inquirer = require('inquirer');
+const fs = require('fs');
+const path = require('path');
+const jest = require('jest');
+
+//constructors
+const Employee = require('./lib/Employee');
+const Engineer = require('./lib/Engineer');
+const Manager = require('./lib/Manager');
+const Intern = require('./lib/Intern');
+
+const DIST_DIR = path.resolve(__dirname, 'dist')
+const outputPath = path.join(DIST_DIR, 'index.html');
+
+const render = require('template.js');
+const { ADDRGETNETWORKPARAMS } = require('dns');
+
+// empty arrays for team and id as place holders
+const teamArr = [];
+const idArr = [];
+
+//start
+function init() {
+    //prompts user for information  to create manaager
+    function addManager() {
+        console.log("Build your team profile");
+        inquirer.prompt([{
+                type: "input",
+                name: "managerName",
+                message: "What's the Manager's name?",
+            },
+            {
+                type: "input",
+                name: "managerId",
+                message: "What is the Manager's ID?"
+            },
+            {
+                type: "input",
+                name: "managerEmail",
+                message: "What is the Manager's Email?"
+            },
+            {
+                type: "input",
+                name: "managerOffNum",
+                message: "What is the Manager's office number?"
+            },
+        ]).then(answer => {
+            const manager = new Manager(answer.managerName, answer.managerId, answer.managerEmail, answer.managerOffNum);
+            teamArr.push(manager);
+            idArr.push(answer.managerId);
+            addTeam();
+        });
+
+    }
+
+    //addTeam function after addManager
+    function addTeam() {
+        //prompt for team members
+        inquirer.prompt([{
+
+        }]);
+    };
+
+}
